d946ac51af98e1177b28bc953274013db1b63316
henryfailbetter <henry@failbettergames.com>
2019-10-04 19:05:58 +0100

    added config file

---
+++ src/Config/Config.js
@@ -0,0 +1,4 @@
+export default {
+    ImageRoot: "https://henrysenior.co.uk/omitf/",
+    ApiRoot: "https://henrysenior.co.uk/omitf/api"
+}
--- src/Home/HomeScreen.js
+++ src/Home/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Text, ImageBackground } from 'react-native';
 import { CustomHeader } from '../Menu/CustomHeader';
+import Config from '../Config/Config'
 
 export default HomeScreen = (props) => 
 {
-  const bgSource = 'https://henrysenior.co.uk/omitf/bg.png';
+  const bgSource = `${Config.ImageRoot}bg.png`;
 
   return (<ImageBackground source={{ uri: bgSource }} style={{ width: '100%', height: '100%' }}>
     <CustomHeader  navigation={props.navigation}></CustomHeader>
